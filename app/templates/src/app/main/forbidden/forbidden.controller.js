0e904d3432ecaeed615f0e73da5896c8e929c009
David Cumps <david@cumps.be>
2014-11-27 10:45:42 +0100

    Initial commit in new repo, coming from https://github.com/infrabel/GNaP.Web.Themes

---
+++ app/templates/src/app/main/forbidden/forbidden.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+    angular
+        .module('<%= appName %>')
+        .controller('MainForbiddenController', MainForbiddenController);
+
+    MainForbiddenController.$inject = [];
+
+    function MainForbiddenController() {
+    }
+})();
